92d899f49adcac13ef30e9edb22d85ebead434b7
Seima-Yonesho <52072148+Seima-Yonesho@users.noreply.github.com>
2019-12-10 20:20:02 +0900

    directory chenge

---
+++ template/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="サイトキャプションを入力">
+  <meta name="keywords" content="サイトキーワードを,で区切って入力">
+  <link rel="stylesheet" href="../pablic/css/toppage.css">
+  <link rel="stylesheet" href="../pablic/css/flame.css">
+  <link rel="stylesheet" href="../template-css/contoroller.css">
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+  <![endif]-->
+  <script src="../pablic/js/jquery-3.4.1.min.js"></script>
+  <script src="../pablic/js/masonry.pkgd.min.js"></script>
+  <script src="../pablic/js/toppage.js"></script>
+</head>
+  <body>
+    <div id="wrapper">
+      <div id="header">
+        <ul>
+          <li><img src=""><a href=""><input type="text" placeholder=""></a></li>
+          <li><a href="">会員登録</a><a href="">ログイン</a></li>
+        </ul>
+      </div>
+      <div id="banner">
+        <p><img src=""></p>
+      </div>
+      <div id="favoriteName">
+        <ul>
+          <li><img src=""></li>
+          <li>shose</li>
+          <li>book</li>
+          <li>cooking</li>
+          <li>toy</li>
+          <li><img src=""></li>
+        </ul>
+      </div>
+      <div id="contents">
+        <div class="box img"><a href=""><img src=""></a></div>
+        <div class="box x2"></div>
+        <div class="box x2"></div>
+        <div class="box x1"></div>
+        <div class="box x1"></div>
+        <div class="box x3"></div>
+        <div class="box l2"></div>
+        <div class="box x1"></div>
+        <div class="box x3"></div>
+        <div class="box x2"></div>
+        <div class="box l2"></div>
+        <div class="box x3"></div>
+        <div class="box x1"></div>
+        <div class="box x3"></div>
+        <div class="box x2"></div>
+        <div class="box x1"></div>
+        <div class="box x1"></div>
+        <div class="box x1"></div>
+        <div class="box x1"></div>
+        <div class="box l2"></div>
+        <div class="box l2"></div>
+        <div class="box x1"></div>
+        <div class="box x1"></div>
+      </div>
+      <div id="controller">
+        <ul>
+          <li><a href=""><img src=""></a></li>
+          <li><a href=""><img src=""></a></li>
+          <li><a href=""><img src=""></a></li>
+          <li><a href=""><img src=""></a></li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
---
+++ template/search/search.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="サイトキャプションを入力">
+  <meta name="keywords" content="サイトキーワードを,で区切って入力">
+  <link rel="stylesheet" href="../pablic/css/flame.css">
+  <link rel="stylesheet" href="../pablic/css/search.css">
+  <link rel="stylesheet" href="../template-css/contoroller.css">
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+  <![endif]-->
+  <script src="../pablic/js/jquery-3.4.1.min.js"></script>
+</head>
+  <body>
+    <div id="wrapper">
+      <div id="header">
+        <ul>
+          <li><img src=""><input type="text" placeholder=""></li>
+          <li><a href="">会員登録</a><a href="">ログイン</a></li>
+        </ul>
+      </div>
+      <div id="popular">
+        <h2>TREND<img src=""></h2>
+        <ul>
+          <li>うんこ</li>
+          <li>ポケモン剣盾</li>
+          <li>Mk</li>
+          <li>徹夜</li>
+        </ul>
+      </div>
+      <div id="category">
+        <h2>CATEGORY<img src=""></h2>
+        <div class="categoryBox">
+        <div class="box">
+          <ul>
+            <li><img src=""></li>
+            <li>SHOSE</li>
+          </ul>
+        </div>
+        <div class="box">
+          <ul>
+            <li><img src=""></li>
+            <li>TOY</li>
+          </ul>
+        </div>
+        <div class="box">
+          <ul>
+             <li><img src=""></li>
+             <li>WEAR</li>
+          </ul>
+        </div>
+        <div class="box">
+          <ul>
+            <li><img src=""></li>
+            <li>BOOK</li>
+          </ul>
+        </div>
+        <div class="box">
+            <ul>
+              <li><img src=""></li>
+              <li>SHOSE</li>
+            </ul>
+          </div>
+          <div class="box">
+            <ul>
+              <li><img src=""></li>
+              <li>TOY</li>
+            </ul>
+          </div>
+          <div class="box">
+            <ul>
+               <li><img src=""></li>
+               <li>WEAR</li>
+            </ul>
+          </div>
+          <div class="box">
+            <ul>
+              <li><img src=""></li>
+              <li>BOOK</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="backButton"><p><a href=""><img src=""></a></p></div>
+      <div id="controller">
+        <ul>
+          <li><a href="#"><img src=""></a></li>
+          <li><a href="#"><img src=""></a></li>
+          <li><a href="#"><img src=""></a></li>
+          <li><a href="#"><img src=""></a></li>
+        </ul>
+      </div>
+    </div>
+  </body>
+</html>
