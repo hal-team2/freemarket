aaa297710e8d6b6973524927b47e623c5063b980
naka <shadowkirby.nagi@gmail.com>
2020-01-25 20:00:47 +0900

    session2

--- function/sell/save_collect_day.php
+++ function/sell/save_collect_day.php
@@ -11,6 +11,7 @@
 
 /*
 発送日時が確定された際にdecide_send_dateを更新する関数
+新たなセッション(出品者id,購入者id)を付与し、購入時に作成した出品者idのセッションは削除
 
 引数：商品ID(str),集荷日(date),集荷時間(str)
 戻り値：なし
@@ -38,8 +39,21 @@ function save_collect_day($product_id,$date,$time){
 /*------------集荷日時書き込み---------------------*/
     $sql = "UPDATE decide_send_date SET decide_date = '".$date."', decide_time = '".$time."' WHERE product_id ='".$product_id."';";
     mysqli_query($cn,$sql);
-    mysqli_close($cn);
+
+/***********session用に取得*************/
+    $sql = "SELECT exhibitor_id,buyer_id FROM products INNER JOIN decide_send_date ON products.id = decide_send_date.product_id WHERE id = '".$product_id."';";
+	$result = mysqli_query($cn,$sql);
+	$row = mysqli_fetch_assoc($result);
+
+	mysqli_close($cn);
+
+	session_start();
+	$_SESSION['buyer'] = $row['buyer_id'];
+	$_SESSION['exhibitor'] = $row['exhibitor_id'];
+	unset($_SESSION['exhibitor_id']);
+
 }
+
 
 
 ?>
